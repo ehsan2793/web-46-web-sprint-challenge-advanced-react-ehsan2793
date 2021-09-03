949c6d1c47a0886d6179653c7c0c202201a5e0c0
ehsan2793 <ehsan2793@gmail.com>
2021-09-03 09:57:00 -0700

    added some new test

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,35 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render , screen} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+render(<CheckoutForm />)
 
-test("shows success message on submit with form details", () => {});
+});
+
+test('when mount header does exixt and it has a text of Checkout Form',()=>{
+    render (<CheckoutForm/>)
+    const header = screen.queryByText(/Checkout Form/i)
+
+    expect(header).toBeInTheDocument()
+    expect(header).toHaveTextContent(/Checkout Form/i)
+    expect(header).toBeTruthy()
+}) 
+
+
+
+
+test("shows success message on submit with form details", () => {
+
+    render (<CheckoutForm/>)
+    const button = screen.getByRole('button')
+    userEvent.click(button)
+    const success =  screen.queryByText(/ You have ordered some plants! Woo-hoo!/i)
+    
+    // expect(header).toHaveTextContent((/ You have ordered some plants! Woo-hoo!/i))
+
+});
