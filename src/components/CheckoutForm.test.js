87615b4173647c85011a501d0ca02c3e2c580ef5
ehsan2793 <ehsan2793@gmail.com>
2021-09-03 11:22:11 -0700

    added more test to to make sure succes massage is showing

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -4,9 +4,15 @@ import { waitFor,render , screen} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 import userEvent from "@testing-library/user-event";
 
+
+/*      Dear grader i put note infront of the two tests i had to run,  to make it easier for you spot. I added some extra test for my own practice,
+    give me some comments on runing test so i cna imporve my preformance.
+    thank ypu 
+ */
+
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {
+test("renders without errors", () => {                                                      // ==> MVP Test
 render(<CheckoutForm />)
 
 });
@@ -33,7 +39,7 @@ test("check out button exist on the page when component is mount",() => {
 
 
 
-test("shows success message on submit with form details",async () => {
+test("shows success message on submit with form details",async () => {                         // ==> MVP Test
 
     render (<CheckoutForm/>)
     const firstName = screen.getByLabelText(/First Name:/i)
@@ -78,11 +84,24 @@ test("shows success message on submit with form details",async () => {
 
         expect(order).toBeInTheDocument
         expect(firstNamefeedback).toBeInTheDocument
+        expect(firstNamefeedback).toHaveTextContent(/ehsan/i)
+
         expect(lastNamefeedback).toBeInTheDocument
+        expect(lastNamefeedback).toHaveTextContent(/rahimi/i)
+
         expect(Addressfeedback).toBeInTheDocument
+        expect(Addressfeedback).toHaveTextContent(/myAddress/i)
+
         expect(cityfeedback).toBeInTheDocument
+        expect(cityfeedback).toHaveTextContent(/fullerton/i)
+
         expect(statefeedback).toBeInTheDocument
+        expect(statefeedback).toHaveTextContent(/ca/i)
+
         expect(zipCodefeedback).toBeInTheDocument
+        expect(zipCodefeedback).toHaveTextContent(/92831/i)
+
+        expect(successMessage).toBeInTheDocument
 
     })
     
